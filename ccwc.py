9396a830888508aa875698c34bbd5573af84ddf5
prasadhbk222 <phadkar@ufl.edu>
2024-02-15 22:57:59 -0800

    complete tool

--- ccwc.py
+++ ccwc.py
@@ -1,55 +1,81 @@
 import argparse
+import sys
 
-def count_bytes(filename):
-    try:
-        with open(filename, 'rb') as file:
-            byte_count = len(file.read())
-            return byte_count
-    except IOError:
-        return "ccwc: {}: No such file or directory".format(filename)
 
-def count_lines(filename):
-    try:
-        with open(filename, 'r') as file:
-            line_count = sum(1 for line in file)
-            return line_count
-    except IOError:
-        return "ccwc: {}: No such file or directory".format(filename)
+def count_bytes(byte_object: bytes):
+    return len(byte_object)
 
-def count_words(filename):
-    try:
-        with open(filename, 'r') as file:
-            word_count = sum(len(line.split()) for line in file)
-            return word_count
-    except IOError:
-        return "ccwc: {}: No such file or directory".format(filename)
+
+def count_lines(byteObject: bytes):
+    return sum(1 for ch in byteObject.decode() if ch == '\n')
+
+
+def count_words(byteObject: bytes):
+    return len(byteObject.decode().split())
+
+
+def count_characters(byteObject):
+
+    return len(byteObject.decode())
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='ccwc - count bytes, lines, or words in a file')
-    parser.add_argument('-c', action='store_true', help='output the number of bytes')
-    parser.add_argument('-l', action='store_true', help='output the number of lines')
-    parser.add_argument('-w', action='store_true', help='output the number of words')
-    parser.add_argument('filename', type=str, help='input file name')
+    parser = argparse.ArgumentParser(
+        description='ccwc - count bytes, lines, or words in a file')
+    parser.add_argument('-c',
+                        action='store_true',
+                        help='output the number of bytes')
+    parser.add_argument('-l',
+                        action='store_true',
+                        help='output the number of lines')
+    parser.add_argument('-w',
+                        action='store_true',
+                        help='output the number of words')
+    parser.add_argument('-m',
+                        action='store_true',
+                        help='output the number of characters')
+
+    parser.add_argument('filename',
+                        nargs='?',
+                        type=str,
+                        help='input file name')
 
     args = parser.parse_args()
+    print(args)
+
+    if args.filename:
+
+        with open(args.filename, 'rb') as file:
+            data = file.read()
+
+    else:
+
+        data = sys.stdin.buffer.read()
 
     if args.c:
-        byte_count = count_bytes(args.filename)
-        if isinstance(byte_count, int):
-            print("{} {}".format(byte_count, args.filename))
-        else:
-            print(byte_count)
+        byte_count = count_bytes(data)
+        print("{} {}".format(byte_count, args.filename))
+
     elif args.l:
-        line_count = count_lines(args.filename)
-        if isinstance(line_count, int):
-            print("{} {}".format(line_count, args.filename))
-        else:
-            print(line_count)
+        line_count = count_lines(data)
+        print("{} {}".format(line_count, args.filename))
+
     elif args.w:
-        word_count = count_words(args.filename)
-        if isinstance(word_count, int):
-            print("{} {}".format(word_count, args.filename))
-        else:
-            print(word_count)
+        word_count = count_words(data)
+        print("{} {}".format(word_count, args.filename))
+
+    elif args.m:
+        char_count = count_characters(data)
+        print("{} {}".format(char_count, args.filename))
+
+    elif not args.c and not args.l and not args.w and not args.m:
+        line_count = count_lines(data)
+        word_count = count_words(data)
+        byte_count = count_bytes(data)
+        print("{} {} {} {}".format(line_count, word_count, byte_count,
+                                   args.filename if args.filename else ''))
+
     else:
-        print("ccwc: please provide either -c, -l, or -w option to count bytes, lines, or words respectively")
+        print(
+            "ccwc: please provide either -c, -l, or -w option to count bytes, lines, or words respectively"
+        )
---
+++ ccwc_test.py
@@ -0,0 +1,17 @@
+import ccwc  # Assuming ccwc.py is in the same directory
+
+def test_count_bytes():
+    byte_data = 'こんにちは、世界！'.encode()
+    assert ccwc.count_bytes(byte_data) == 27
+
+def test_count_lines():
+    byte_data = b'This is a\nsample\nbyte sequence.\n'
+    assert ccwc.count_lines(byte_data) == 3
+
+def test_count_words():
+    byte_data = b'This is a sample byte sequence.'
+    assert ccwc.count_words(byte_data) == 6
+
+def test_count_characters():
+    byte_data = 'こんにちは、世界！'.encode()
+    assert ccwc.count_characters(byte_data) == 9
